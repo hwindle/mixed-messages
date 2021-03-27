4acd6f9b0aec89a3a8bc86a998b45ee076e38e14
Hazel Windle <hwindle83@googlemail.com>
2021-03-27 14:08:30 +0000

    new object with 4 arrays for the animals, food, moods and animal pics. Added animal pics.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
-console.log('hello');
+msgBits = {
+  animals = [
+    'cow',
+    'sheep',
+    'chicken',
+    'duck',
+    'horse',
+    'arctic seal',
+    'giraffe',
+    'rainbow trout'
+  ],
+  foods = [
+    'grass',
+    'lettuce',
+    'sweetcorn',
+    'ham',
+    'pasta bake',
+    'weeds',
+    'flies',
+    'shrimp'
+  ],
+  feelings = [
+    'unhappy 🙁',
+    'happy 🙂',
+    'depressed 😩',
+    'angry 😠',
+    'silly 😛',
+    'elated 😄'
+  ],
+  picUrls = [
+    'pics/01_cow.jpg',
+    'pics/02_sheep.jpg',
+    'pics/03_chicken.jpg',
+    'pics/04_duck.jpg',
+    'pics/05_horse.jpg',
+    'pics/06_seal.jpg',
+    'pics/07_giraffe.jpg',
+    'pics/08_trout.jpg'
+  ]
+};
